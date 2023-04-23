991429812f543f8699e5895bdafb7f989e87e467
derinasrudin1 <deri.nasrudin78@gmail.com>
2023-04-23 19:57:16 +0700

    rubah style

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let hasil1 = document.getElementById('hasil1');
-let hasil2 = document.getElementById('hasil2');
-let ngitung = document.getElementById('hitung');
-ngitung.addEventListener('click', function () {
+const hasil1 = document.getElementById('hasil1');
+const hasil2 = document.getElementById('hasil2');
+const ngitung = document.getElementById('hitung');
+ngitung.addEventListener('click', () => {
   let a = document.getElementById('nilai1').value;
   let b = document.getElementById('nilai2').value;
   let c = document.getElementById('nilai3').value;
-  let diskriminan = Math.pow(b, 2) - 4 * a * c;
+  let diskriminan = Math.pow(b, 2) - 4 * (a * c);
   let d = Math.sqrt(diskriminan);
   let pembagi = 2 * a;
   if (diskriminan > 0) {
@@ -20,5 +20,10 @@ ngitung.addEventListener('click', function () {
     hasil2.innerHTML = 'Nilai x2 nya adalah' + ' ' + x2;
   } else if (diskriminan < 0) {
     hasil1.innerHTML = 'Nilai x1,x2 nya adalah akar imajiner  ' + -b + '±' + '√' + diskriminan + '/' + pembagi;
+    hasil2.innerHTML = ' ';
   }
 });
+
+function refres() {
+  window.location.reload();
+}
