3d89d95fd27f6ed435a1a93bd6df3d9da1275485
derinasrudin1 <deri.nasrudin78@gmail.com>
2023-07-03 21:26:54 +0700

    tambah kode latex dihasil ketika D<0

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
-const hasil1 = document.getElementById('hasil1');
-const hasil2 = document.getElementById('hasil2');
-const ngitung = document.getElementById('hitung');
-ngitung.addEventListener('click', () => {
-  let a = document.getElementById('nilai1').value;
-  let b = document.getElementById('nilai2').value;
-  let c = document.getElementById('nilai3').value;
-  let diskriminan = Math.pow(b, 2) - 4 * (a * c);
-  let d = Math.sqrt(diskriminan);
-  let pembagi = 2 * a;
+const hasil1 = document.getElementById("hasil1");
+const hasil2 = document.getElementById("hasil2");
+const ngitung = document.getElementById("hitung");
+
+ngitung.addEventListener("click", () => {
+  const a = parseFloat(document.getElementById("nilai1").value);
+  const b = parseFloat(document.getElementById("nilai2").value);
+  const c = parseFloat(document.getElementById("nilai3").value);
+
+  const diskriminan = Math.pow(b, 2) - 4 * a * c;
+  const d = Math.sqrt(diskriminan);
+  const pembagi = 2 * a;
+
   if (diskriminan > 0) {
-    let x1 = (-b + d) / pembagi;
-    let x2 = (-b - d) / pembagi;
-    hasil1.innerHTML = 'Nilai x1 nya adalah' + ' ' + x1;
-    hasil2.innerHTML = 'Nilai x2 nya adalah' + ' ' + x2;
-  } else if (diskriminan == 0) {
-    let x1 = (-b + d) / pembagi;
-    let x2 = x1;
-    hasil1.innerHTML = 'Nilai x1 nya adalah' + ' ' + x1;
-    hasil2.innerHTML = 'Nilai x2 nya adalah' + ' ' + x2;
-  } else if (diskriminan < 0) {
-    hasil1.innerHTML = 'Nilai x1,x2 nya adalah akar imajiner  ' + -b + '±' + '√' + diskriminan + '/' + pembagi;
-    hasil2.innerHTML = ' ';
+    const x1 = (-b + d) / pembagi;
+    const x2 = (-b - d) / pembagi;
+
+    hasil1.innerHTML = "Nilai \\(x_1\\) nya adalah " + x1;
+    hasil2.innerHTML = "Nilai \\(x_2\\) nya adalah " + x2;
+    MathJax.typeset([hasil1, hasil2]);
+  } else if (diskriminan === 0) {
+    const x1 = (-b + d) / pembagi;
+
+    hasil1.innerHTML = "Nilai \\(x_1\\) nya adalah " + x1;
+    hasil2.innerHTML = "Nilai \\(x_2\\) nya adalah " + x1;
+    MathJax.typeset([hasil1, hasil2]);
+  } else {
+    hasil1.innerHTML = `Nilai \\(x_1\\), \\(x_2\\) nya adalah akar imajiner -${b} \\(\\pm\\) \\(\\sqrt{${diskriminan}\\over {${pembagi}}}\\)`;
+    MathJax.typeset([hasil1]);
   }
 });
 
